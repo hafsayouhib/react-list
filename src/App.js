458947211ddfd69a98a0f276be2825d40521bd35
hafsayouhib <148340527+hafsayouhib@users.noreply.github.com>
2024-03-08 18:56:35 +0500

    initial

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css'
+import React from 'react';
+// import Header from'./components/header/Header'
+// import Main from './components/Main';
+// import Footer from'./components/Footer'
+// import Menulist from './components/list/Nav'
+import List from './components/List'
+
+function App() {
+  return (
+   <>
+   <h1>My React List App</h1>
+      <List />
+   </>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import heading from './Header.module.css'
+
+
+
+const Header = ()=>{
+    return(
+<header className={heading}>
+    <h1>welcome I am from Header </h1>
+
+    </header>
+    );
+};
+export default Header;
